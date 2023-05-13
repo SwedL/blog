578d753c9e401385629a3fe8a05f130c8c652734
autotrade <swedl@yandex.ru>
2023-05-13 16:09:51 +0500

    second commit

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index_info),
     path('<int:number>', views.get_info_number),
     path('<str:post>', views.get_info_post)
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index_info(request):
+    return render(request, 'posts/list_detail.html')
+
 def get_info_number(request, number):
     return HttpResponse(f'Здесь содержится информация о посте под номером {number}')
-
 
 def get_info_post(request, post):
     return HttpResponse(f'Информация о посте {post}')

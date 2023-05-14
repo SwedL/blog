54ad7db53525b96df4d8ec16acd12895342ceaaf
autotrade <swedl@yandex.ru>
2023-05-14 17:16:41 +0500

    render and templates

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index_info),
+    path('kiany', views.info_kiany),
+    path('records', views.get_guinness_world_records),
     path('<int:number>', views.get_info_number),
-    path('<str:post>', views.get_info_post)
+    path('<str:name>', views.get_info_post)
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,8 +5,31 @@ from django.http import HttpResponse
 def index_info(request):
     return render(request, 'posts/list_detail.html')
 
-def get_info_number(request, number):
-    return HttpResponse(f'Здесь содержится информация о посте под номером {number}')
+def info_kiany(request):
+    data = {
+        'year_born': '1964',
+        'city_born': 'Бейрут',
+        'movie_name': 'На гребне волны',
+    }
+    return render(request, 'posts/info_kiany.html', context=data)
 
-def get_info_post(request, post):
-    return HttpResponse(f'Информация о посте {post}')
+def get_guinness_world_records(request):
+    context = {
+        'power_man': 'Narve Laeret',
+        'bar_name': 'Bob’s BBQ & Grill',
+        'count_needle': 1790,
+    }
+    return render(request, 'posts/guinnessworldrecords.html', context=context)
+
+
+def get_info_number(request, number):
+    data = {
+        'number': number,
+    }
+    return render(request, 'posts/detail_by_number.html', context=data)
+
+def get_info_post(request, name):
+    data = {
+        'name': name
+    }
+    return render(request, 'posts/detail_by_name.html', context=data)
